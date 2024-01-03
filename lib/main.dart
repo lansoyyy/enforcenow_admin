31ff916bb2b3bbb1b06d160f8af8967ea8768271
Lans12345 <olanalans12345@gmail.com>
2024-01-03 15:58:59 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:enforcenow_admin/screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'enforcenow-65366.firebaseapp.com',
+          apiKey: "AIzaSyC5VgTLgDFKYtEeXneycIz_fyW_qm1pF6s",
+          appId: "1:617661924245:web:e598b027a3e9c1d30cec9c",
+          messagingSenderId: "617661924245",
+          projectId: "enforcenow-65366",
+          storageBucket: "enforcenow-65366.appspot.com"));
   runApp(const MyApp());
 }
 
