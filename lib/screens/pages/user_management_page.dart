59d1dc8036e28fe5d780659f75867291894423bc
Lans12345 <olanalans12345@gmail.com>
2024-01-03 15:14:25 +0800

    added user management ui

--- lib/screens/pages/user_management_page.dart
+++ lib/screens/pages/user_management_page.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 import '../../widgets/drawer_widget.dart';
 import '../../widgets/text_widget.dart';
 
-class UserManagementPage extends StatelessWidget {
+class UserManagementPage extends StatefulWidget {
   const UserManagementPage({super.key});
 
+  @override
+  State<UserManagementPage> createState() => _UserManagementPageState();
+}
+
+class _UserManagementPageState extends State<UserManagementPage> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,82 +29,121 @@ class UserManagementPage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: DataTable(
-          columns: [
-            DataColumn(
-                label: TextBold(text: 'ID', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label:
-                    TextBold(text: 'Name', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label: TextBold(
-                    text: 'Contact Number', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label: TextBold(
-                    text: 'Address', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label:
-                    TextBold(text: 'Email', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label:
-                    TextBold(text: 'Type', fontSize: 18, color: Colors.black)),
-            DataColumn(
-                label: TextBold(text: '', fontSize: 18, color: Colors.black)),
-          ],
-          rows: [
-            for (int i = 0; i < 50; i++)
-              DataRow(cells: [
-                DataCell(
-                  TextRegular(
-                    text: '${i + 1}',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 45,
+                width: 300,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(5)),
+                child: TextFormField(
+                  onChanged: (value) {
+                    setState(() {
+                      nameSearched = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      hintText: 'Search User',
+                      hintStyle: TextStyle(fontFamily: 'QRegular'),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      )),
+                  controller: searchController,
                 ),
-                DataCell(
-                  TextRegular(
-                    text: 'John Doe',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-                DataCell(
-                  TextRegular(
-                    text: '09090104355',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-                DataCell(
-                  TextRegular(
-                    text: 'Nazareth, CDO',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-                DataCell(
-                  TextRegular(
-                    text: 'johndoe@gmail.com',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-                DataCell(
-                  TextRegular(
-                    text: 'Enforcer',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-                DataCell(IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.delete,
-                    color: Colors.red,
-                  ),
-                )),
-              ]),
-          ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DataTable(
+                columns: [
+                  DataColumn(
+                      label: TextBold(
+                          text: 'ID', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: 'Name', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: 'Contact Number',
+                          fontSize: 18,
+                          color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: 'Address', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: 'Email', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: 'Type', fontSize: 18, color: Colors.black)),
+                  DataColumn(
+                      label: TextBold(
+                          text: '', fontSize: 18, color: Colors.black)),
+                ],
+                rows: [
+                  for (int i = 0; i < 50; i++)
+                    DataRow(cells: [
+                      DataCell(
+                        TextRegular(
+                          text: '${i + 1}',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(
+                        TextRegular(
+                          text: 'John Doe',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(
+                        TextRegular(
+                          text: '09090104355',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(
+                        TextRegular(
+                          text: 'Nazareth, CDO',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(
+                        TextRegular(
+                          text: 'johndoe@gmail.com',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(
+                        TextRegular(
+                          text: 'Enforcer',
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                      DataCell(IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                      )),
+                    ]),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
